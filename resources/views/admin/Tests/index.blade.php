4c68498f25392726a3e2a0d7cc688e535f36ea80
Mukhtar Isah <mukhtarisah6@gmail.com>
2024-02-14 13:50:05 +0100

    add to index tes

--- resources/views/admin/Tests/index.blade.php
+++ resources/views/admin/Tests/index.blade.php
@@ -17,5 +17,41 @@
     @empty
         
     @endforelse
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Course Name</th>
+                <th>Tests</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($courses as $course)
+                 <tr>
+                    <td>{{ $course->name }}</td>
+                    <td>
+                        @if($course->tests->count() > 0)
+                            <ul>
+                                @foreach($course->tests as $test)
+                                    <li>{{ $test->name }}</li>
+                                @endforeach
+                            </ul>
+                        @else
+                            No tests available
+                        @endif
+                    </td>
+                    <td>
+                        <a href="{{ route('courses.edit', $course->id) }}" class="btn btn-primary">Edit</a>
+                        <form action="{{ route('courses.destroy', $course->id) }}" method="POST" style="display: inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+             @endforeach
+        </tbody>
+    </table>
+
 
 @endsection
