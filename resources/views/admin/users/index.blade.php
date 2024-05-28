b1682cd64484a778a8d8d1c02b50f33a8f409ac1
Mukhtar Isah <mukhtarisah6@gmail.com>
2024-05-28 16:58:20 +0100

    fix admin view

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,15 +2,14 @@
 @section('content')
     <div class="container">
         <h1>Users</h1>
-        <a href="{{ route('users.create') }}" class="btn btn-primary">Add User</a>
+        <a href="/register" class="btn btn-primary">Add User</a>
         <table class="table mt-3">
             <thead>
                 <tr>
                     <th>Name</th>
                     <th>Email</th>
                     <th>Admin</th>
-                    <th>Reg No</th>
-                    <th>Level</th>
+                    
                     <th>Actions</th>
                 </tr>
             </thead>
@@ -20,10 +19,10 @@
                         <td>{{ $user->name }}</td>
                         <td>{{ $user->email }}</td>
                         <td>{{ $user->is_admin ? 'Yes' : 'No' }}</td>
-                        <td>{{ $user->reg_no }}</td>
-                        <td>{{ $user->level }}</td>
+                        
+                        
                         <td>
-                            <a href="{{ route('users.show', $user->id) }}" class="btn btn-info">View</a>
+                            
                             <a href="{{ route('users.edit', $user->id) }}" class="btn btn-warning">Edit</a>
                             <form action="{{ route('users.destroy', $user->id) }}" method="POST" style="display:inline;">
                                 @csrf
