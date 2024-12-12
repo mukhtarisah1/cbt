3f219e43d98590ee7d5d638309619885a17f7b99
mukhtar isah <mukhtarisah6@gmail.com>
2024-12-12 10:33:07 +0100

    fixed student dashboard

--- resources/views/students/dashboard.blade.php
+++ resources/views/students/dashboard.blade.php
@@ -4,26 +4,25 @@
 <div class="card text-center p-4">
     <div class="card-title"><h3 class="text-success">Welcome to your dasboard</h3></div>
     <div class="card-body">
-        <h5>You have the following pending test(s), click on any to begin  </h5>
-            <div>
-                @if(count($testsId) > 0)
-                <h6 class="mt-3">Your Assigned Tests:</h6>
-                <ul>
-                    @foreach($testsId as $test)
-                        <li class="mb-2" style="list-style-type: none;">
-                            Test: {{ $test->test->title }}
-                            <form action="{{ route('students.tests.startInstructions', ['test' => $test->test_id]) }}" method="get">
-                                @csrf
-                                <button type="submit" class="btn btn-primary">Take Test</button>
-                            </form>
-                        </li>
-                    @endforeach
-                </ul>
-            @else
-                <p>No tests assigned to you.</p>
-            @endif
-            </div>
+    <h5>You have the following pending test(s), click on any to begin</h5>
+    <div>
+            <h6 class="mt-3">Your Assigned Tests:</h6>
+            <ul>
+                @forelse($testsId as $test)
+                    <li class="mb-2" style="list-style-type: none;">
+                        Test: {{ $test->test->title }}
+                        <form action="{{ route('students.tests.startInstructions', ['test' => $test->test_id]) }}" method="get">
+                            @csrf
+                            <button type="submit" class="btn btn-primary">Take Test</button>
+                        </form>
+                    </li>
+                @empty
+                    <p>No tests assigned to you.</p>
+                @endforelse
+            </ul>
+        </div>
     </div>
+
 </div>
     
 
